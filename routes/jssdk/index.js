e1b75b588aebbe8a7b2088f6cddb36f86684eafb
wfatec <chao125010@163.com>
2019-06-28 11:19:45 +0800

    remove console

--- routes/jssdk/index.js
+++ routes/jssdk/index.js
@@ -1,7 +1,5 @@
 const jssdkRoute = router => {
     router.post('/jssdk/publish', async (ctx, next) => {
-        console.log(ctx.request.body)
-
         return ctx.body = ctx.request.body
     });
 }
