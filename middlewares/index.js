3c7587308a994dace557f98dc95c5c022b5637e9
wfatec <chao125010@163.com>
2019-07-01 00:03:15 +0800

    add error catch

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,24 @@
-const bodyParser = require('koa-bodyparser');
-const httpError = require('./http-error');
+const bodyParser = require("koa-bodyparser");
+const staticFiles = require("koa-static");
+const path = require("path");
+const nunjucks = require("koa-nunjucks-2")
+const httpError = require("./http-error");
 
 module.exports = app => {
-    app.use(httpError());
-    app.use(bodyParser());
-}
+  app.use(
+    httpError({
+      errorPageFolder: path.resolve(__dirname, "../errorPage")
+    })
+  );
+  app.use(staticFiles(path.resolve(__dirname, "../public")));
+  app.use(
+    nunjucks({
+      ext: "html",
+      path: path.join(__dirname, "../views"),
+      nunjucksConfig: {
+        trimBlocks: true
+      }
+    })
+  );
+  app.use(bodyParser());
+};
