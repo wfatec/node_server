11dd60e5ee100481dac242a04fe1441b29f27a16
wfatec <chao125010@163.com>
2019-07-01 00:18:43 +0800

    add logs

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,24 @@
 const bodyParser = require("koa-bodyparser");
 const staticFiles = require("koa-static");
+const ip = require("ip");
 const path = require("path");
-const nunjucks = require("koa-nunjucks-2")
+const nunjucks = require("koa-nunjucks-2");
 const httpError = require("./http-error");
+const log = require("./log");
 
 module.exports = app => {
   app.use(
     httpError({
       errorPageFolder: path.resolve(__dirname, "../errorPage")
+    })
+  );
+  app.use(
+    log({
+      env: app.env,
+      projectName: "sdk",
+      appLogLevel: "debug",
+      dir: "logs",
+      serverIp: ip.address()
     })
   );
   app.use(staticFiles(path.resolve(__dirname, "../public")));
