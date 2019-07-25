399e7373fb4728131eab722e5ff8b267941f7682
wfatec <chao125010@163.com>
2019-07-25 18:24:39 +0800

    update cors

--- middlewares/index.js
+++ middlewares/index.js
@@ -38,21 +38,7 @@ module.exports = app => {
       serverIp: ip.address()
     })
   );
-  app.use(
-    cors({
-      origin: function(ctx) {
-        if (ctx.url === "/uba") {
-          return "*"; // 允许来自所有域名请求
-        }
-        // return 'http://localhost:8080'; // 这样就能只允许 http://localhost:8080 这个域名的请求了
-      },
-      exposeHeaders: ["WWW-Authenticate", "Server-Authorization"],
-      maxAge: 5,
-      credentials: true,
-      allowMethods: ["GET", "POST", "DELETE"],
-      allowHeaders: ["Content-Type", "Authorization", "Accept"]
-    })
-  );
+  app.use(cors());
   app.use(staticFiles(path.resolve(__dirname, "../public")));
   app.use(
     nunjucks({
